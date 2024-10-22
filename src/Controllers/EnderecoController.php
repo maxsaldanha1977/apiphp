7f138394a35872ae245967cf43ec43cec3dc7e41
Max Saldanha <maxuelsaldanha@gmail.com>
2024-10-22 14:57:25 -0300

    databasescript

---
+++ src/Controllers/EnderecoController.php
@@ -0,0 +1,154 @@
+<?php 
+
+namespace App\Controllers;
+
+use App\Http\Request;
+use App\Http\Response;
+use App\Services\EnderecoService;
+
+class EnderecoController
+{
+    public function store(Request $request, Response $response)
+    {
+        $body = $request::body();
+
+        $enderecoService = EnderecoService::create($body);
+
+        if (isset($enderecoService['error'])) {
+            return $response::json([
+                'error'   => true,
+                'success' => false,
+                'message' => $enderecoService['error']
+            ], 400);
+        }
+
+        $response::json([
+            'error'   => false,
+            'success' => true,
+            'data'    => $enderecoService
+        ], 201);
+    }
+
+  
+    public function fetch(Request $request, Response $response)
+    {
+        $authorization = $request::authorization();
+
+        $enderecoService = EnderecoService::fetch($authorization);
+
+        if (isset($enderecoService['unauthorized'])) {
+            return $response::json([
+                'error'   => true,
+                'success' => false,
+                'message' => $enderecoService['unauthorized']
+            ], 401);
+        }
+
+        if (isset($enderecoService['error'])) {
+            return $response::json([
+                'error'   => true,
+                'success' => false,
+                'message' => $enderecoService['error']
+            ], 400);
+        }
+
+        $response::json([
+            'error'   => false,
+            'success' => true,
+            'data'    => $enderecoService
+        ], 200);
+        return;
+    }
+
+    public function fetchAll(Request $request, Response $response)
+    {
+        $authorization = $request::authorization();
+
+        $enderecoService = EnderecoService::fetchAll($authorization);
+
+        if (isset($enderecoService['unauthorized'])) {
+            return $response::json([
+                'error'   => true,
+                'success' => false,
+                'message' => $enderecoService['unauthorized']
+            ], 401);
+        }
+
+        if (isset($enderecoService['error'])) {
+            return $response::json([
+                'error'   => true,
+                'success' => false,
+                'message' => $enderecoService['error']
+            ], 400);
+        }
+
+        $response::json([
+            'error'   => false,
+            'success' => true,
+            'data'    => $enderecoService
+        ], 200);
+        return;
+    }
+
+    public function update(Request $request, Response $response)
+    {
+        $authorization = $request::authorization();
+
+        $body = $request::body();
+
+        $enderecoService = EnderecoService::update($authorization, $body);
+
+        if (isset($enderecoService['unauthorized'])) {
+            return $response::json([
+                'error'   => true,
+                'success' => false,
+                'message' => $enderecoService['unauthorized']
+            ], 401);
+        }
+
+        if (isset($enderecoService['error'])) {
+            return $response::json([
+                'error'   => true,
+                'success' => false,
+                'message' => $enderecoService['error']
+            ], 400);
+        }
+
+        $response::json([
+            'error'   => false,
+            'success' => true,
+            'message' => $enderecoService
+        ], 200);
+        return;
+    }
+
+    public function remove(Request $request, Response $response, array $id)
+    {
+        $authorization = $request::authorization();
+
+        $enderecoService = EnderecoService::delete($authorization, $id[0]);
+
+        if (isset($enderecoService['unauthorized'])) {
+            return $response::json([
+                'error'   => true,
+                'success' => false,
+                'message' => $enderecoService['unauthorized']
+            ], 401);
+        }
+
+        if (isset($enderecoService['error'])) {
+            return $response::json([
+                'error'   => true,
+                'success' => false,
+                'message' => $enderecoService['error']
+            ], 400);
+        }
+
+        $response::json([
+            'error'   => false,
+            'success' => true,
+            'message' => $enderecoService
+        ], 200);
+        return;
+    }
+}
---
+++ src/Models/Endereco.php
@@ -0,0 +1,105 @@
+<?php 
+
+namespace App\Models;
+
+use App\Models\Database;
+use PDO;
+
+class Cliente extends Database
+{
+    public static function save(array $data)
+    {
+        $pdo = self::getConnection();
+
+        $stmt = $pdo->prepare("
+            INSERT 
+            INTO 
+                endereco (rua, cidade, estado, cep)
+            VALUES
+                (?, ?, ?, ?)
+        ");
+
+        $stmt->execute([
+            $data['rua'],
+            $data['cidade'],
+            $data['estado'],
+            $data['cep']
+        ]);
+
+        return $pdo->lastInsertId() > 0 ? true : false;
+    }
+   
+
+    public static function find(int|string $id)
+    {
+        $pdo = self::getConnection();
+
+        $stmt = $pdo->prepare('
+            SELECT 
+                id, rua, cidade, estado, cep
+            FROM 
+                endereco
+            WHERE 
+                id = ?
+        ');
+
+        $stmt->execute([$id]);
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+    public static function findAll()
+    {
+        $pdo = self::getConnection();
+
+        $stmt = $pdo->prepare('
+            SELECT 
+                id, rua, cidade, estado, cep
+            FROM 
+                endereco
+        ');
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function update(int|string $id, array $data)
+    {
+        $pdo = self::getConnection();
+        
+        $stmt = $pdo->prepare('
+            UPDATE 
+                endereco
+            SET 
+               rua = ?, 
+               cidade = ?, 
+               estado = ?, 
+               cep = ?
+            WHERE 
+                id = ?
+        ');
+
+        $stmt->execute([$data['rua'], $data['cidade'], $data['estado'],  $data['enderoco_id'], $id]);  //Defini os campos que serão afetados para UserService.php
+
+        return $stmt->rowCount() > 0 ? true : false;
+    }
+
+    public static function delete(int|string $id)
+    {
+        $pdo = self::getConnection();
+
+        $stmt = $pdo->prepare('
+            DELETE 
+            FROM 
+                endereco
+            WHERE 
+                id = ?
+        ');
+
+        $stmt->execute([$id]);
+
+        return $stmt->rowCount() > 0 ? true : false;
+    }
+}
