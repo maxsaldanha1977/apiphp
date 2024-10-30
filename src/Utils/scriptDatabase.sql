656ea96895285b9674539f3b86f2fa2a49484bf6
Max Saldanha <maxuelsaldanha@gmail.com>
2024-10-30 09:29:32 -0300

    :broom: cleanup: Eliminando blocos de código comentados e variáveis não utilizadas na função de validação de formulário

--- src/Utils/scriptDatabase.sql
+++ src/Utils/scriptDatabase.sql
@@ -1,23 +1,23 @@
 
 CREATE TABLE IF NOT EXISTS users (
-id SERIAL PRIMARY KEY,
-name VARCHAR(155) NOT null,
-email VARCHAR(255) UNIQUE NOT null,
-password VARCHAR(255) NOT null
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(155) NOT null,
+    email VARCHAR(255) UNIQUE NOT null,
+    password VARCHAR(255) NOT null
 );
 
 CREATE TABLE IF NOT EXISTS endereco (
-id SERIAL PRIMARY KEY,
-logradouro VARCHAR(255),
-numero VARCHAR(10),
-cidade VARCHAR(100),
-estado VARCHAR(100)
+    id SERIAL PRIMARY KEY,
+    logradouro VARCHAR(255),
+    numero VARCHAR(10),
+    cidade VARCHAR(100),
+    estado VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS cliente (
-id SERIAL PRIMARY KEY,
-nome VARCHAR(255),
-email VARCHAR(255),
-endereco_id INT,
-FOREIGN KEY (endereco_id) REFERENCES endereco(id)
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    email VARCHAR(255),
+    endereco_id INT,
+    FOREIGN KEY (endereco_id) REFERENCES endereco(id)
 );
